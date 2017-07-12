d112e72c1b643f4e85910dde12cfd65facd225de
singzinc <sing.zinc.hk@gmail.com>
2017-07-12 23:24:54 +0800

    operator
    how to define the operators

---
+++ tut9.R
@@ -0,0 +1,15 @@
+# how to define the operators
+
+########### example 1 #############
+"%,%" <- function(x, y) paste(x, y, sep = ",")
+
+# test run
+
+"Hello" %,% "World"
+
+
+########### example 2 ##########
+
+10 %% 2 == `%%`(10, 2)
+"hello" %in% c("hello", "world") == `%in%`("hello", c("hello", "world"))
+10 + 2 == `+`(10, 2)
